3df1fed83e4dea3a73ed354c2f7781e864484bbf
RubisRebelle <103526691+RubisRebelle@users.noreply.github.com>
2022-06-16 06:19:53 +0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+export const columns = [
+  {
+    name: "Name",
+    selector: "Name",
+    sortable: true
+  },
+  {
+    name: "Position",
+    selector: "position",
+    sortable: true
+  },
+  {
+    name: "Office",
+    selector: "office",
+    sortable: true
+  },
+  {
+    name: "Age",
+    selector: "age",
+    sortable: true
+  },
+  {
+    name: "Start date",
+    selector: "start_date",
+    sortable: true
+  },
+  {
+    name: "Salary",
+    selector: "salary",
+    sortable: true
+  }
+];
+
+export const data = [
+  {
+    name: "Tiger Nixon",
+    position: "System Architect",
+    office: "Edinburgh",
+    age: "61",
+    start_date: "2011/04/25",
+    salary: "$320,800"
+  },
+  {
+    name: "Garett Winters",
+    position: "Accountant",
+    office: "Tokyo",
+    age: "63",
+    start_date: "2011/07/25",
+    salary: "$170,750"
+  }
+];
